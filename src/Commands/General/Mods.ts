cce827fc29858a78a0c9795e28c234e1951a2952
Toshi さん <91844428+Toshi-san001@users.noreply.github.com>
2022-08-19 00:26:17 +0530

    Update Mods.ts

--- src/Commands/General/Mods.ts
+++ src/Commands/General/Mods.ts
@@ -16,7 +16,7 @@ export default class extends BaseCommand {
         for (let i = 0; i < this.client.config.mods.length; i++)
             text += `\n*#${i + 1}*\n🧧 *Username:* ${
                 this.client.contact.getContact(this.client.config.mods[i]).username
-            }\n 🌀*Contact:* @${M.sender.jid.split('@')[0]}*`
+            }\n 🌀*Contact:* https://wa.me/+${this.client.config.mods[i].split('@')[0]}*`
         return void (await reply(text))
     }
 }
