a2b6b9fb58f0fae697324e1df674363c1577589d
Toshi さん <91844428+Toshi-san001@users.noreply.github.com>
2022-08-20 20:52:22 +0530

    Update Help.ts

--- src/Commands/General/Help.ts
+++ src/Commands/General/Help.ts
@@ -70,22 +70,6 @@ export default class extends BaseCommand {
 
 📕 Note: Use ${this.client.config.prefix}help <command_name> for more info of a specific command. Example: ${this.client.config.prefix}help hello
 `
-            return void (await M.reply(text, 'text', undefined, undefined, undefined, [M.sender.jid]))
-        } else {
-            const cmd = context.trim().toLowerCase()
-            const command = this.handler.commands.get(cmd) || this.handler.aliases.get(cmd)
-            if (!command) return void M.reply(`No command found | "${context.trim()}"`)
-            return void M.reply(
-                `🎐 Command: ${this.client.utils.capitalize(command.name)}\n🌀 Aliases: ${
-                    !command.config.aliases
-                        ? ''
-                        : command.config.aliases.map((alias) => this.client.utils.capitalize(alias)).join(', ')
-                }\n🏮 Category: ${this.client.utils.capitalize(command.config.category)}\n⏰ Cooldown: ${
-                    command.config.cooldown ?? 3
-                }s\n💡 Usage: ${command.config.usage
-                    .split('||')
-                    .map((usage) => `${this.client.config.prefix}${usage.trim()}`)
-                    .join(' | ')}\n🧧 Description: ${command.config.description}`
             )
         }
     }
