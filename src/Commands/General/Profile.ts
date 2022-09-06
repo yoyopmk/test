2a3561bda040a368f08e0da99808d84ef947d5c0
Toshi さん <91844428+Toshi-san001@users.noreply.github.com>
2022-09-07 00:34:59 +0530

    Update Profile.ts

--- src/Commands/General/Profile.ts
+++ src/Commands/General/Profile.ts
@@ -2,44 +2,42 @@ import { BaseCommand, Command, Message } from '../../Structures'
 import { getStats } from '../../lib'
 
 @Command('profile', {
-    description: "Displays user's profile",
+    description: '',
     category: 'general',
-    aliases: ['p'],
+    exp: 25,
     cooldown: 15,
-    exp: 30,
-    usage: 'profile [tag/quote users]'
+    aliases: ['p'],
+    usage: ''
 })
-export default class extends BaseCommand {
-    public override execute = async (M: Message): Promise<void> => {
-        const users = M.mentioned
-        if (M.quoted && !users.includes(M.quoted.sender.jid)) users.push(M.quoted.sender.jid)
-        while (users.length < 1) users.push(M.sender.jid)
-        const user = users[0]
-        const username = user === M.sender.jid ? M.sender.username : this.client.contact.getContact(user).username
-        let pfpUrl: string | undefined
+export default class command extends BaseCommand {
+    override execute = async ({ mentioned, quoted, sender, reply, groupMetadata }: Message): Promise<void> => {
+        const users = mentioned
+        if (quoted && !users.includes(quoted.sender.jid)) users.push(quoted.sender.jid)
+        while (users.length < 1) users.push(sender.jid)
+        let pfp!: Buffer
         try {
-            pfpUrl = await this.client.profilePictureUrl(user, 'image')
-        } catch {
-            pfpUrl = undefined
+            pfp = await this.client.utils.getBuffer((await this.client.profilePictureUrl(users[0], 'image')) || '')
+        } catch (error) {
+            pfp = this.client.assets.get('404') as Buffer
         }
-        const pfp = pfpUrl ? await this.client.utils.getBuffer(pfpUrl) : (this.client.assets.get('404') as Buffer)
+        const info = await this.client.DB.getUser(users[0])
+        let username: string = this.client.contact.getContact(users[0]).username
+        if (users[0] === sender.jid) username = sender.username
+        const haigusha = info.haigusha.married ? info.haigusha.data.name : 'None'
+        const admin = this.client.utils.capitalize(`${groupMetadata?.admins?.includes(users[0]) || false}`)
+        const ban = !info.banned || info.banned === null ? 'False' : 'True'
+        const { rank } = getStats(info.level)
         let bio!: string
         try {
-            bio = (await this.client.fetchStatus(user))?.status || ''
+            bio = (await this.client.fetchStatus(users[0]))?.status || ''
         } catch (error) {
             bio = ''
         }
-        const { banned, experience, level, tag } = await this.client.DB.getUser(user)
-        const admin = this.client.utils.capitalize(`${M.groupMetadata?.admins?.includes(user) || false}`)
-        const { rank } = getStats(level)
-        return void M.reply(
-            pfp,
-            'image',
-            undefined,
-            undefined,
-            `🏮 *Username:* ${username}#${tag}\n\n🎫 *Bio:* ${bio}\n\n🌟 *Experience:* ${experience}\n\n🥇 *Rank:* ${rank}\n\n🍀 *Level:* ${level}\n\n👑 *Admin:* ${admin}\n\n🟥 *Banned:* ${this.client.utils.capitalize(
-                `${banned || false}`
-            )}`
-        )
+        const text = `🀄 *Username:* ${username}#${info.tag}\n\n🔰*Bio:* ${bio}\n\n🏮*XP:* ${
+            info.experience || 0
+        }\n\n⚡*Rank:* ${rank}\n\n🌀*Haigusha:* ${haigusha}\n\n💮*Pokemon:* ${
+            info.party.length + info.pc.length
+        }\n\n*Quiz Wins:* ${info.quizWins}\n\n*Admin:* ${admin}\n\n*Banned:* ${ban || 'False'}`
+        return void (await reply(pfp, 'image', undefined, undefined, text))
     }
 }
