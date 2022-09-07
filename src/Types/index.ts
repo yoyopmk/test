f872b5b916c3bd5bbb70879af4eb209edaaae293
Toshi さん <91844428+Toshi-san001@users.noreply.github.com>
2022-09-08 00:48:38 +0530

    Update index.ts

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -3,6 +3,7 @@ import Baileys, { GroupMetadata, ParticipantAction } from '@adiwajshing/baileys'
 export * from './Config'
 export * from './Command'
 export * from './Message'
+export * from './Pokemon'
 
 export interface IContact {
     jid: string
@@ -24,6 +25,7 @@ export enum GroupFeatures {
     'events' = 'By enabling this feature, the bot will welcome new members, gives farewell to the members who left the group or were removed and reacts when a member is promoted or demoted',
     'mods' = "By enabling this feature, it enables the bot to remove the member (except for admins) which sent an invite link of other groups. This will work if and only if the bot's an admin",
     'nsfw' = 'By enabling this feature, it enables the bot to send *NSFW* contents'
+    'wild' = 'By enabling this feature, it will send wild pokemon',
 }
 
 export interface IGroup extends GroupMetadata {
