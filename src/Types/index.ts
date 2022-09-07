ef3949f1ac5ad66878df51a76d8ecdbb01a93259
Toshi さん <91844428+Toshi-san001@users.noreply.github.com>
2022-09-08 00:59:44 +0530

    Update index.ts

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -25,7 +25,7 @@ export enum GroupFeatures {
     'events' = 'By enabling this feature, the bot will welcome new members, gives farewell to the members who left the group or were removed and reacts when a member is promoted or demoted',
     'mods' = "By enabling this feature, it enables the bot to remove the member (except for admins) which sent an invite link of other groups. This will work if and only if the bot's an admin",
     'nsfw' = 'By enabling this feature, it enables the bot to send *NSFW* contents'
-    'wild' = 'By enabling this feature, it will send wild pokemon',
+    'wild' = 'By enabling this feature, it will send wild pokemon'
 }
 
 export interface IGroup extends GroupMetadata {
