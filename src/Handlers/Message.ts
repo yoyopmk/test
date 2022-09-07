25cfaa44d2f1095e8e59e464810b0fa52cdb866a
Toshi さん <91844428+Toshi-san001@users.noreply.github.com>
2022-09-07 21:23:34 +0530

    Update Message.ts

--- src/Handlers/Message.ts
+++ src/Handlers/Message.ts
@@ -1,9 +1,13 @@
 import { join } from 'path'
 import { readdirSync } from 'fs-extra'
 import chalk from 'chalk'
+import { schedule } from 'node-cron'
+import { ICharacter as WaifuResponse, Character } from '@shineiichijo/marika'
+import { IQuiz } from 'anime-quiz'
 import { Message, Client, BaseCommand } from '../Structures'
 import { getStats } from '../lib'
-import { ICommand, IArgs } from '../Types'
+import { ICommand, IArgs, IPokemonAPIResponse } from '../Types'
+import { Pokemon } from '../Database'
 
 export class MessageHandler {
     constructor(private client: Client) {}
@@ -72,7 +76,7 @@ export class MessageHandler {
     public handleMessage = async (M: Message): Promise<void> => {
         const { prefix } = this.client.config
         const args = M.content.split(' ')
-        const title = M.chat === 'group' ? M.groupMetadata?.subject || 'Group' : 'DM'
+        const title = M.chat === 'dm' ? 'DM' : M.groupMetadata?.subject || 'Group'
         await this.moderate(M)
         if (!args[0] || !args[0].startsWith(prefix))
             return void this.client.log(
@@ -83,44 +87,51 @@ export class MessageHandler {
         this.client.log(
             `${chalk.cyanBright(`Command ${args[0]}[${args.length - 1}]`)} from ${chalk.yellowBright(
                 M.sender.username
-            )} in ${chalk.blueBright(`${title}`)}`
+            )} in ${chalk.blueBright(title)}`
         )
+        const { bot } = await this.client.DB.getGroup(M.from)
+        const commands = ['switch', 'hello', 'hi']
         const { banned, tag } = await this.client.DB.getUser(M.sender.jid)
-        if (banned) return void M.reply('You are banned from using commands')
         if (!tag)
             await this.client.DB.updateUser(M.sender.jid, 'tag', 'set', this.client.utils.generateRandomUniqueTag())
         const cmd = args[0].toLowerCase().slice(prefix.length)
+        if (bot != this.client.config.name.split(' ')[0] && bot !== 'all' && !commands.includes(cmd)) return void null
+        if (banned) return void M.reply(`You are banned from using commands`)
         const command = this.commands.get(cmd) || this.aliases.get(cmd)
         if (!command) return void M.reply('No such command, Baka!')
         const disabledCommands = await this.client.DB.getDisabledCommands()
         const index = disabledCommands.findIndex((CMD) => CMD.command === command.name)
         if (index >= 0)
             return void M.reply(
-                `*${this.client.utils.capitalize(cmd)}* is currently disabled by *${
-                    disabledCommands[index].disabledBy
-                }* in *${disabledCommands[index].time} (GMT)*. ❓ *Reason:* ${disabledCommands[index].reason}`
+                `*${this.client.utils.capitalize(cmd)}* is disabled by *${disabledCommands[index].disabledBy}* in *${
+                    disabledCommands[index].time
+                } (GMT)*. Reason: *${disabledCommands[index].reason}*`
             )
-        if (command.config.category === 'dev' && !this.client.config.mods.includes(M.sender.jid))
-            return void M.reply('This command can only be used by the MODS')
-        if (M.chat === 'dm' && !command.config.dm) return void M.reply('This command can only be used in groups')
+        if (M.chat === 'dm' && !command.config.dm) return void M.reply('You can use this command only in groups')
         if (command.config.category === 'moderation' && !M.sender.isAdmin)
             return void M.reply('This command can only be used by the group admins')
-        const { nsfw } = await this.client.DB.getGroup(M.from)
-        if (command.config.category === 'nsfw' && !nsfw)
-            return void M.reply('This command can only be used in NSFW enabled groups')
+        if (command.config.category === 'nsfw' && !(await this.client.DB.getGroup(M.from)).nsfw)
+            return void M.reply('This comand can only be used in NSFW enabled groups')
+        if (command.config.category === 'dev' && !M.sender.isMod)
+            return void M.reply('This command can only be used by the MODS')
+        if (command.config.casino && M.from !== this.client.config.casinoGroup)
+            return void M.reply(`This command can only be used in the casino group`)
+        const isAdmin = M.groupMetadata?.admins?.includes(this.client.correctJid(this.client.user?.id || ''))
+        if (command.config.adminRequired && !isAdmin) return void M.reply('make me admin')
         const cooldownAmount = (command.config.cooldown ?? 3) * 1000
         const time = cooldownAmount + Date.now()
         if (this.cooldowns.has(`${M.sender.jid}${command.name}`)) {
             const cd = this.cooldowns.get(`${M.sender.jid}${command.name}`)
             const remainingTime = this.client.utils.convertMs((cd as number) - Date.now())
             return void M.reply(
-                `You are on a cooldown. Wait *${remainingTime}* ${
+                `You are on a cooldown of this command. Wait *${remainingTime}* ${
                     remainingTime > 1 ? 'seconds' : 'second'
-                } before using this command again`
+                }`
             )
         } else this.cooldowns.set(`${M.sender.jid}${command.name}`, time)
         setTimeout(() => this.cooldowns.delete(`${M.sender.jid}${command.name}`), cooldownAmount)
-        await this.client.DB.setExp(M.sender.jid, command.config.exp || 10)
+        const exp = command.config.exp ?? 10
+        await this.client.DB.setExp(M.sender.jid, exp)
         await this.handleUserStats(M)
         try {
             await command.execute(M, this.formatArgs(args))
@@ -129,12 +140,109 @@ export class MessageHandler {
         }
     }
 
+    public loadWildEnabledGroups = async (): Promise<void> => {
+        const groups = !this.groups ? await this.client.getAllGroups() : this.groups
+        for (const group of groups) {
+            const data = await this.client.DB.getGroup(group)
+            if (!data.wild) continue
+            this.wild.push(group)
+        }
+        this.client.log(
+            `Successfully loaded ${chalk.blueBright(`${this.wild.length}`)} ${
+                this.wild.length > 1 ? 'groups' : 'group'
+            } which has enabled wild`
+        )
+        await this.spawnPokemon()
+    }
+
+    public loadCharaEnabledGroups = async (): Promise<void> => {
+        const groups = !this.groups ? await this.client.getAllGroups() : this.groups
+        for (const group of groups) {
+            const data = await this.client.DB.getGroup(group)
+            if (!data.chara) continue
+            this.chara.push(group)
+        }
+        this.client.log(
+            `Successfully loaded ${chalk.blueBright(`${this.chara.length}`)} ${
+                this.chara.length > 1 ? 'groups' : 'group'
+            } which has enabled chara`
+        )
+        await this.spawnChara()
+    }
+
+    private spawnChara = async (): Promise<void> => {
+        schedule('*/5 * * * *', async () => {
+            if (this.chara.length < 1) return void null
+            for (let i = 0; i < this.chara.length; i++) {
+                setTimeout(async () => {
+                    const { chara, bot } = await this.client.DB.getGroup(this.wild[i])
+                    if (bot !== 'all' && bot !== this.client.config.name.split(' ')[0]) return void null
+                    if (!chara) return void null
+                    await new Character()
+                        .getRandomCharacter()
+                        .then(async (chara) => {
+                            const price = Math.floor(Math.random() * (50000 - 25000) + 25000)
+                            let source = ''
+                            await new Character()
+                                .getCharacterAnime(chara.mal_id)
+                                .then((res) => (source = res.data[0].anime.title))
+                                .catch(async () => {
+                                    await new Character()
+                                        .getCharacterManga(chara.mal_id.toString())
+                                        .then((res) => (source = res.data[0].manga.title))
+                                        .catch(() => {})
+                                })
+                            const buffer = await this.client.utils.getBuffer(chara.images.jpg.image_url)
+                            const buttons = [
+                                {
+                                    buttonId: 'id1',
+                                    buttonText: { displayText: `${this.client.config.prefix}claim` },
+                                    type: 1
+                                }
+                            ]
+                            const buttonMessage = {
+                                image: buffer,
+                                caption: `*A claimable character Appeared!*\n\n🏮 *Name: ${chara.name}*\n\n📑 *About:* ${chara.about}\n\n🌐 *Source: ${source}*\n\n💰 *Price: ${price}*\n\n*[Use ${this.client.config.prefix}claim to have this character in your gallery]*`,
+                                footer: '',
+                                buttons: buttons,
+                                headerType: 4
+                            }
+                            this.charaResponse.set(this.chara[i], { price, data: chara })
+                            await this.client.sendMessage(this.chara[i], buttonMessage)
+                        })
+                        .catch(() => {})
+                }, (i + 1) * 20 * 1000)
+            }
+        })
+    }
+
     private moderate = async (M: Message): Promise<void> => {
-        if (M.chat !== 'group') return void null
+        if (M.chat !== 'group') {
+            const urls = M.urls
+            if (!urls.length) return void null
+            const groupinvites = urls.filter((url) => url.includes('chat.whatsapp.com'))
+            if (!groupinvites.length) return void null
+            this.client.log(
+                `${chalk.blueBright('GROUP REQUEST')} from ${chalk.yellowBright(
+                    M.sender.username
+                )} in ${chalk.cyanBright('DM')}`
+            )
+            const text = `Request from *@${M.sender.jid.split('@')[0]}*\n\n${M.content}`
+            if (M.message.key.fromMe) return void null
+            await this.client.sendMessage(this.client.config.adminsGroup, {
+                text,
+                mentions: [M.sender.jid]
+            })
+            return void M.reply('Your request has been sent')
+        }
         const { mods } = await this.client.DB.getGroup(M.from)
-        const isAdmin = M.groupMetadata?.admins?.includes(this.client.correctJid(this.client.user?.id || ''))
-        if (!mods || M.sender.isAdmin || !isAdmin) return void null
-        const urls = this.client.utils.extractUrls(M.content)
+        if (
+            !mods ||
+            M.sender.isAdmin ||
+            !M.groupMetadata?.admins?.includes(this.client.correctJid(this.client.user?.id || ''))
+        )
+            return void null
+        const urls = M.urls
         if (urls.length > 0) {
             const groupinvites = urls.filter((url) => url.includes('chat.whatsapp.com'))
             if (groupinvites.length > 0) {
@@ -142,10 +250,11 @@ export class MessageHandler {
                     const code = await this.client.groupInviteCode(M.from)
                     const inviteSplit = invite.split('/')
                     if (inviteSplit[inviteSplit.length - 1] !== code) {
+                        const title = M.groupMetadata?.subject || 'Group'
                         this.client.log(
                             `${chalk.blueBright('MOD')} ${chalk.green('Group Invite')} by ${chalk.yellow(
                                 M.sender.username
-                            )} in ${chalk.cyanBright(M.groupMetadata?.subject || 'Group')}`
+                            )} in ${chalk.cyanBright(title)}`
                         )
                         return void (await this.client.groupParticipantsUpdate(M.from, [M.sender.jid], 'remove'))
                     }
@@ -153,44 +262,6 @@ export class MessageHandler {
             }
         }
     }
-  spawnPokemon = async (): Promise<void> => {
-    cron.schedule("*/2 * * * *", async () => {
-      const Data = await await this.client.getFeatures("pokemon");
-      if (Data.id === "000") return void null;
-      const p = Math.floor(Math.random() * Data.jids.length);
-      const q = await this.client.getGroupData(Data.jids[p]);
-      if (!q.wild || q.bot !== this.client.user.name) return void null;
-      const i = Math.floor(Math.random() * 898);
-      const y = Math.floor(Math.random() * 100);
-      const { data } = await axios.get(
-        `https://pokeapi.co/api/v2/pokemon/${i}`
-      );
-      const buffer = await this.client.getBuffer(
-        `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`
-      );
-      await this.client.DB.group.updateMany(
-        { jid: Data.jids[p] },
-        {
-          $set: {
-            catchable: true,
-            lastPokemon: data.name,
-            pId: data.id,
-            pLevel: y,
-            pImage: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`,
-          },
-        }
-      );
-      await this.client.sendMessage(Data.jids[p], buffer, MessageType.image, {
-        caption: `A wild pokemon appeared! Use ${this.client.config.prefix}catch to catch this pokemon.`,
-      });
-      setTimeout(async () => {
-        await this.client.DB.group.updateOne(
-          { jid: Data.jids[p] },
-          { $set: { catchable: false } }
-        );
-      }, 500000);
-    });
-  };
 
     private formatArgs = (args: string[]): IArgs => {
         args.splice(0, 1)
@@ -242,4 +313,18 @@ export class MessageHandler {
     private cooldowns = new Map<string, number>()
 
     private path = [__dirname, '..', 'Commands']
+
+    public pokemonResponse = new Map<string, Pokemon>()
+
+    public pokemonTradeResponse = new Map<string, { offer: Pokemon; creator: string; with: string }>()
+
+    public haigushaResponse = new Map<string, WaifuResponse>()
+
+    public charaResponse = new Map<string, { price: number; data: WaifuResponse }>()
+
+    public quiz = {
+        quizResponse: new Map<string, IQuiz>(),
+        failed: new Map<string, string[]>(),
+        creator: new Map<string, string>()
+    }
 }
