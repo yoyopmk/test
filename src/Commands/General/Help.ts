f11b5038eec35a0e09cff402117b490f79b16391
Toshi さん <91844428+Toshi-san001@users.noreply.github.com>
2022-09-07 12:06:01 +0530

    Update Help.ts

--- src/Commands/General/Help.ts
+++ src/Commands/General/Help.ts
@@ -18,45 +18,107 @@ export default class extends BaseCommand {
             })).filter((command) => command.data.config.category !== 'dev')
             const { nsfw } = await this.client.DB.getGroup(M.from)
             if (!nsfw) commands = commands.filter(({ data }) => data.config.category !== 'nsfw')
-            let text = `konnichiwa
+            let text = `(⁠◍⁠•⁠ᴗ⁠•⁠◍⁠)⁠❤ konnichiwa 👋🏻 @${M.sender.jid.split('@')[0]} I'm ${this.client.config.name !
+            
+📫My prefix-  ${this.client.config.prefix}         
+                
             
 *━━━━━『•Fun•』━━━━━*
 
-⌬ ${this.client.config.prefix}friendship
-⌬ ${this.client.config.prefix}reaction
-⌬ ${this.client.config.prefix}ship
-⌬ ${this.client.config.prefix}simp
-⌬ ${this.client.config.prefix}triggered
+🔰friendship
+🀄Discription:- Calculates the level of a friendship!
+🧧Usage:- ${this.client.config.prefix}frendship
+
+🔰reaction
+🀄Discription:- React via anime gifs with the tagged or quoted user!
+🧧Usage:- ${this.client.config.prefix}reaction
+
+🔰ship
+🀄Discription:- Ship People ❤️!
+🧧Usage:- ${this.client.config.prefix}ship 
+
+🔰simp
+🀄Discription:- Makes a person simp!
+🧧Usage:- ${this.client.config.prefix}simp
+
+🔰triggered
+🀄Discription:- Makes a triggered gif of the tagged/quoted user or the provided/quoted image!
+🧧Usage:- ${this.client.config.prefix}triggered
 
 *━━━━━『•General•』━━━━━*
 
-⌬ ${this.client.config.prefix}help
-⌬ ${this.client.config.prefix}hi
-⌬ ${this.client.config.prefix}info
-⌬ ${this.client.config.prefix}mods
-⌬ ${this.client.config.prefix}profile
-⌬ ${this.client.config.prefix}rank
+🔰help
+🀄Discription:- Displays the bot's usable commands
+🧧Usage:- ${this.client.config.prefix}help
+
+🔰hi
+🀄Discription:- Says hello to the bot
+🧧Usage:- ${this.client.config.prefix}hi
+
+🔰info
+🀄Discription:- Displays bot's info!
+🧧Usage:- ${this.client.config.prefix}info
+
+🔰mods
+🀄Discription:- Displays the bot's moderators!
+🧧Usage:- ${this.client.config.prefix}mods
+
+🔰profile
+🀄Discription:- Displays user's profile!
+🧧Usage:- ${this.client.config.prefix}profile
+ 
+🔰rank
+🀄Discription:- Displays user's rank
+🧧Usage:- ${this.client.config.prefix}rank
 
 *━━━━━『•Media•』━━━━━*
 
-⌬ ${this.client.config.prefix}lurics
-⌬ ${this.client.config.prefix}play
-⌬ ${this.client.config.prefix}spotify
-⌬ ${this.client.config.prefix}yta
-⌬ ${this.client.config.prefix}yts
-⌬ ${this.client.config.prefix}ytv
+🔰lyrics 
+🀄Discription:- Sends the lyrics of a given song!
+🧧Usage:- ${this.client.config.prefix}lyrics
+
+🔰play
+🀄Discription:- Plays a song of the given term from YouTube!
+🧧Usage:- ${this.client.config.prefix}play
+
+🔰Spotify
+🀄Discription:- Downloads and sends the track of thr given spotify track URL!
+🧧Usage:- ${this.client.config.prefix}Spotify
+
+🔰yta
+🀄Discription:-  Downloads and sends the video as an audio of the provided YouTube video link!
+🧧Usage:- ${this.client.config.prefix}yta
+
+🔰yts
+🀄Discription:- Searches the video of the given query in YouTube!
+🧧Usage:- ${this.client.config.prefix}yts
+
+🔰ytv
+🀄Discription:- Downloads and sends the video of the provided YouTube video link!
+🧧Usage:- ${this.client.config.prefix}ytv
 
 *━━━━━『•Moderation•』━━━━━*
 
-⌬ ${this.client.config.prefix}ping
-⌬ ${this.client.config.prefix}set
+💮ping
+💮set
 
 *━━━━━『•Utils•』━━━━━*
 
-⌬ ${this.client.config.prefix}prettier
-⌬ ${this.client.config.prefix}react
-⌬ ${this.client.config.prefix}retrieve
-⌬ ${this.client.config.prefix}sticker
+🔰prettire
+🀄Discription:- Runs prettier of the given code!
+🧧Usage:- ${this.client.config.prefix}prettire
+
+🔰react
+🀄Discription:- Reacts a message with the given emoji!
+🧧Usage:- ${this.client.config.prefix}react
+
+🔰retrieve
+🀄Discription:- Retrieves view once message!
+🧧Usage:- ${this.client.config.prefix}retrieve
+
+🔰sticker
+🀄Discription:- Converts image/video/gif to sticker!
+🧧Usage:- ${this.client.config.prefix}sticker 
 
 *━━━━━『•Weeb•』━━━━━*
 
