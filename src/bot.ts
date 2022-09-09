554cea920a32a5687ffdd5f371d361a4e5809558
Toshi さん <91844428+Toshi-san001@users.noreply.github.com>
2022-09-10 03:57:52 +0530

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -1,25 +1,37 @@
 import { Client } from './Structures'
-import { MessageHandler, AssetHandler, CallHandler, EventHandler } from './Handlers'
-;(async (): Promise<void> => {
+import { MessageHandler, AssetHandler, CallHandler, EventHandler, ModHandler } from './Handlers'
+;(async () => {
     const client = new Client()
 
     await client.start()
 
     new AssetHandler(client).loadAssets()
 
-    const { handleMessage, loadCommands } = new MessageHandler(client)
+    const messageHandler = new MessageHandler(client)
 
-    const { handleEvents, sendMessageOnJoiningGroup } = new EventHandler(client)
+    const { handleEvents, sendMessageOnJoiningGroup } = new EventHandler(client, messageHandler)
 
     const { handleCall } = new CallHandler(client)
 
-    loadCommands()
+    const modHandler = new ModHandler(client)
 
-    client.on('new_message', async (M) => await handleMessage(M))
+    messageHandler.loadCommands()
+
+    client.on('new_message', async (M) => await messageHandler.handleMessage(M))
 
     client.on('participants_update', async (event) => await handleEvents(event))
 
-    client.on('new_group_joined', async (group) => await sendMessageOnJoiningGroup(group))
+    client.on('new_group_joined', async (group) => {
+        messageHandler.groups.push(group.jid)
+        await sendMessageOnJoiningGroup(group)
+    })
 
     client.on('new_call', async (call) => await handleCall(call))
+
+    client.once('open', async () => {
+        messageHandler.groups = await client.getAllGroups()
+        await messageHandler.loadWildEnabledGroups()
+        await messageHandler.loadCharaEnabledGroups()
+        await modHandler.loadMods()
+    })
 })()
