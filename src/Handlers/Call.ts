ca298ac0fd5ee3092291436898057384ce6e5ff8
Toshi さん <91844428+Toshi-san001@users.noreply.github.com>
2022-09-10 04:04:09 +0530

    Update Call.ts

--- src/Handlers/Call.ts
+++ src/Handlers/Call.ts
@@ -1,16 +1,15 @@
 import chalk from 'chalk'
-import { WACallEvent } from '@adiwajshing/baileys'
 import { Client } from '../Structures'
 
 export class CallHandler {
     constructor(private client: Client) {}
 
-    public handleCall = async (call: WACallEvent): Promise<void> => {
+    public handleCall = async (call: { from: string }): Promise<void> => {
         const caller = call.from
         const { username } = this.client.contact.getContact(caller)
         this.client.log(`${chalk.cyanBright('Call')} from ${chalk.blueBright(username)}`)
         await this.client.sendMessage(caller, { text: 'You are now banned' })
-        await this.client.DB.updateBanStatus(caller)
+        await this.client.DB.updateBanStatus(caller, 'ban')
         return void (await this.client.updateBlockStatus(caller, 'block'))
     }
 }
