ea5eb78067d55b5a57ef2faf0d346b9d8fb61b27
Toshi さん <91844428+Toshi-san001@users.noreply.github.com>
2022-09-10 04:30:47 +0530

    Update _Command_Example_.ts

--- src/Commands/_Command_Example_.ts
+++ src/Commands/_Command_Example_.ts
@@ -3,7 +3,7 @@ import { IArgs } from '../Types'
 
 @Command('command_name', {
     description: 'Description of the command',
-    Tcategory: 'Tcategory',
+    category: 'Tcategory',
     usage: 'example on using the command',
     cooldown: 5,
     exp: 10,
