553a64b420a76a1312c97ec29bfa2274dba66399
Toshi さん <91844428+Toshi-san001@users.noreply.github.com>
2022-09-09 13:14:55 +0530

    Update User.ts

--- src/Database/Models/User.ts
+++ src/Database/Models/User.ts
@@ -1,12 +1,88 @@
-import { prop, getModelForClass } from '@typegoose/typegoose'
+import { prop, getModelForClass, modelOptions, Severity } from '@typegoose/typegoose'
 import { Document } from 'mongoose'
+import { ICharacter, ICharacter as WaifuResponse } from '@shineiichijo/marika'
 
-export class UserSchema {
+export class Pokemon {
+    @prop({ type: String, required: true })
+    public name!: string
+
+    @prop({ type: String, required: true })
+    public image!: string
+
+    @prop({ type: Number, required: true })
+    public id!: number
+
+    @prop({ type: Number, required: true })
+    public level!: number
+}
+
+@modelOptions({
+    options: {
+        allowMixed: Severity.ALLOW
+    }
+})
+class Haigusha {
+    @prop({ type: Boolean, required: true, default: false })
+    public married!: boolean
+
+    @prop({ type: Object, required: true, default: () => ({}) })
+    public data!: WaifuResponse
+}
+
+@modelOptions({
+    options: {
+        allowMixed: Severity.ALLOW
+    }
+})
+class Gallery implements ICharacter {
+    @prop({ required: true })
+    public mal_id!: ICharacter['mal_id']
+
+    @prop({ type: String, required: true })
+    public url!: string
+
+    @prop({ type: Object, required: true })
+    public images!: ICharacter['images']
+
+    @prop({ type: String, required: true })
+    public name!: string
+
+    @prop({ type: () => [String], required: true, default: [] })
+    public nicknames!: string[]
+
+    @prop({ type: String, required: true })
+    public about!: string
+
+    @prop({ required: true })
+    public favorites!: number
+}
+
+@modelOptions({
+    options: {
+        allowMixed: Severity.ALLOW
+    }
+})
+export class User {
     @prop({ type: String, required: true, unique: true })
     public jid!: string
 
     @prop({ type: Number, required: true, default: 0 })
     public experience!: number
+
+    @prop({ type: Number, required: true, default: 0 })
+    public wallet!: number
+
+    @prop({ type: Number, required: true, default: 0 })
+    public bank!: number
+
+    @prop({ type: Number, required: true, default: 0 })
+    public quizWins!: number
+
+    @prop({ type: Number, required: true, default: 0 })
+    public lastDaily!: number
+
+    @prop({ type: Number, required: true, default: 0 })
+    public lastRob!: number
 
     @prop({ type: Boolean, required: true, default: false })
     public banned!: boolean
@@ -16,20 +92,20 @@ export class UserSchema {
 
     @prop({ type: String, required: true })
     public tag!: string
-    
-    @prop({ type: Boolean, required: true, default: false })
-    public wild!: boolean
 
-    @prop({ type: Number, required: true, default: 0 })
-    public wallet!: number
+    @prop({ type: () => Pokemon, required: true, default: [] })
+    public party!: Pokemon[]
 
-    @prop({ type: Number, required: true, default: 0 })
-    public bank!: number
+    @prop({ type: () => Pokemon, required: true, default: [] })
+    public pc!: Pokemon[]
 
-    @prop({ type: Number, required: true, default: 0 })
-    public lastDaily!: number
+    @prop({ type: () => Haigusha, required: true, default: () => ({ married: false, data: {} }) })
+    public haigusha!: Haigusha
+
+    @prop({ type: () => Gallery, required: true, default: [] })
+    public gallery!: Gallery[]
 }
 
-export type TUserModel = UserSchema & Document
+export type TUserModel = User & Document
 
-export const userSchema = getModelForClass(UserSchema)
+export const userSchema = getModelForClass(User)
