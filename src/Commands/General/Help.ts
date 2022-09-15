3be0328a342b795f38505d987bbe7311d08dd3ee
♕︎Toshi_さん♕︎ <91844428+Toshi-san001@users.noreply.github.com>
2022-09-15 12:12:38 +0530

    Update Help.ts

--- src/Commands/General/Help.ts
+++ src/Commands/General/Help.ts
@@ -18,8 +18,8 @@ export default class extends BaseCommand {
             })).filter((command) => command.data.config.category !== 'dev')
             const { nsfw } = await this.client.DB.getGroup(M.from)
             if (!nsfw) commands = commands.filter(({ data }) => data.config.category !== 'nsfw')
-            let text = `🎉Hi there! 👋🏻 _@${M.sender.jid.split('@')[0]}_, I'm *${
-                this.client.config.name*
+            let text = `🎉Hi there! 👋🏻 _@${M.sender.jid.split('@')[0]}_, I'm ${
+                this.client.config.name
             }\n prefix - "${this.client.config.prefix}"\n\n📝 *Note* This is my usable menu list and I'm here to to help with my menu list.`
             const categories: string[] = []
             for (const command of commands) {
