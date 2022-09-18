840948032bc61ae1aceca9c50e9f1e7391eb92bd
RedBeeツ <77657831+bilal-08@users.noreply.github.com>
2022-09-18 16:25:08 +0530

    Update Message.ts

--- src/Handlers/Message.ts
+++ src/Handlers/Message.ts
@@ -92,12 +92,12 @@ export class MessageHandler {
 try {
         const { bot } = await this.client.DB.getGroup(M.from)
         const commands = ['switch', 'hello', 'hi']
-       // const { banned, tag } = await this.client.DB.getUser(M.sender.jid)
-        //if (!tag)
-           // await this.client.DB.updateUser(M.sender.jid, 'tag', 'set', this.client.utils.generateRandomUniqueTag())
+        const { banned, tag } = await this.client.DB.getUser(M.sender.jid)
+        if (!tag)
+            await this.client.DB.updateUser(M.sender.jid, 'tag', 'set', this.client.utils.generateRandomUniqueTag())
         const cmd = args[0].toLowerCase().slice(prefix.length)
         if (bot != this.client.config.name.split(' ')[0] && bot !== 'all' && !commands.includes(cmd)) return void null
-    //    if (banned) return void M.reply(`You are banned from using commands`)
+       if (banned) return void M.reply(`You are banned from using commands`)
         const command = this.commands.get(cmd) || this.aliases.get(cmd)
         if (!command) return void M.reply(`No, such a command type || *${this.client.config.prefix}help* to know more`)
         const disabledCommands = await this.client.DB.getDisabledCommands()
@@ -131,10 +131,10 @@ try {
             )
         } else this.cooldowns.set(`${M.sender.jid}${command.name}`, time)
         setTimeout(() => this.cooldowns.delete(`${M.sender.jid}${command.name}`), cooldownAmount)
-        //await this.client.DB.setExp(M.sender.jid, command.config.exp || 10)
-        //const exp = command.config.exp ?? 10
-        //await this.client.DB.setExp(M.sender.jid, exp)
-       // await this.handleUserStats(M)
+        await this.client.DB.setExp(M.sender.jid, command.config.exp || 10)
+        const exp = command.config.exp ?? 10
+        await this.client.DB.setExp(M.sender.jid, exp)
+        await this.handleUserStats(M)
         
             await command.execute(M, this.formatArgs(args))
         } catch (error) {
