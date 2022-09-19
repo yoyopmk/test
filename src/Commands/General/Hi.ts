15f9cdae56048c7c37b3e5dfdac05f5cad3a6608
♕︎Toshi_さん♕︎ <91844428+Toshi-san001@users.noreply.github.com>
2022-09-19 10:12:48 +0530

    Update Hi.ts

--- src/Commands/General/Hi.ts
+++ src/Commands/General/Hi.ts
@@ -9,21 +9,6 @@ import { BaseCommand, Command, Message } from '../../Structures'
     cooldown: 5
 })
 export default class extends BaseCommand {
-    public override execute = async ({ sender, reply }: Message): Promise<void> => {
-                  const buttons = [
-                                {
-                                    buttonId: `${this.client.config.prefix}help`,
-                                    buttonText: { displayText: `${this.client.config.prefix}help` },
-                                    type: 1
-                                }
-                            ]
-                            const buttonMessage = {
-                                caption: `Baileys Button Test ✅`,
-                                footer: '🤖 Baileys',
-                                buttons: buttons,
-                                headerType: 4
-                            }
-                          void (await this.client.sendMessage(from, buttonMessage))
-                        });
-       //void (await reply(`Hello! *${sender.username}* Darling`))
+    public override execute = async ({ sender, reply }: Message): Promise<void> =>
+        void (await reply(`Hello! *${sender.username}*`))
 }
