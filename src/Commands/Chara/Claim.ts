43faf19ff31cac0316d1682a198fec0af738a3c2
♕︎Toshi_さん♕︎ <91844428+Toshi-san001@users.noreply.github.com>
2022-09-24 17:00:49 +0530

    Update Claim.ts

--- src/Commands/Chara/Claim.ts
+++ src/Commands/Chara/Claim.ts
@@ -12,7 +12,7 @@ export default class extends BaseCommand {
         const res = this.handler.charaResponse.get(M.from)
         if (!res) return void M.reply('no chara to claim')
         const { wallet, gallery } = await this.client.DB.getUser(M.sender.jid)
-        if (res.price > wallet) return void M.reply('Check your wallet')
+        if (res.price > wallet) return void M.reply(`Lol Check your wallet you don't have such gold to claim this`)
         this.handler.charaResponse.delete(M.from)
         await this.client.DB.updateUser(M.sender.jid, 'wallet', 'inc', -res.price)
         gallery.push(res.data)
