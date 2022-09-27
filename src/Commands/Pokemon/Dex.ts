b854d2a1c4d41abd359319147f119cfb22c66de3
♕︎Toshi_さん♕︎ <91844428+Toshi-san001@users.noreply.github.com>
2022-09-28 00:02:57 +0530

    Update Dex.ts

--- src/Commands/Pokemon/Dex.ts
+++ src/Commands/Pokemon/Dex.ts
@@ -16,7 +16,7 @@ export default class command extends BaseCommand {
         const buffer = await this.client.utils.getBuffer('https://telegra.ph/file/4dc8912fc40a24b7c61f4.jpg')
         const pokemons = [...party, ...pc]
         if (pokemons.length < 1) return void M.reply('no pokemon')
-        let text = `*⭐ Pokedex ⭐*\n`
+        let text = `*🔰 Pokedex 🔰*\n`
         pokemons.forEach((x) => (text += `\n*❯ ${this.client.utils.capitalize(x.name)}*`))
         return void (await M.reply(buffer, 'image', true, undefined, text, [M.sender.jid]))
     }
