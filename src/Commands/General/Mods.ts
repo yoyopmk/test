846d773a36bafa2f26e9b0c617e70c2fb6acb968
♕︎Toshi_さん♕︎ <91844428+Toshi-san001@users.noreply.github.com>
2022-09-28 11:07:47 +0530

    Update Mods.ts

--- src/Commands/General/Mods.ts
+++ src/Commands/General/Mods.ts
@@ -12,7 +12,7 @@ import { Message, Command, BaseCommand } from '../../Structures'
 export default class extends BaseCommand {
     public override execute = async (M: Message): Promise<void> => {
         if (!this.client.config.mods.length) return void M.reply('*[UNMODERATED]*')
-        let text =" *▬▬▬〖･BOT MODS･〗▬▬▬*\n\n"
+        let text =" *▬〖･BOT MODS･〗▬*\n\n"
 let mentions:string[] = []
 this.client.config.mods.map((x)=>{
 text+= `*❯ @${this.client.contact.getContact(x).jid.split('@')[0]}*\n`
