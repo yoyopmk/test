246aabe8639ebb309a923dac1404493a13045c8b
♕︎Toshi_さん♕︎ <91844428+Toshi-san001@users.noreply.github.com>
2022-09-28 11:10:12 +0530

    Update Help.ts

--- src/Commands/General/Help.ts
+++ src/Commands/General/Help.ts
@@ -1,51 +1,41 @@
-import { proto } from '@adiwajshing/baileys'
-import { Message, BaseCommand, Command } from '../../Structures'
-import { IArgs, GroupFeatures } from '../../Types'
+import { BaseCommand, Command, Message } from '../../Structures'
+import { IArgs } from '../../Types'
 
 @Command('help', {
-    description: 'Enables/Disables a certain group feature',
-    usage: 'help',
-    cooldown: 1,
-    category: 'moderation',
-    exp: 25,
-    aliases: ['feature']
+    description: "Displays the bot's usable commands",
+    aliases: ['h'],
+    cooldown: 10,
+    exp: 20,
+    usage: 'help || help <command_name>',
+    category: 'general'
 })
 export default class extends BaseCommand {
-    public override execute = async (M: Message, { flags }: IArgs): Promise<void> => {
-        const features = Object.keys(GroupFeatures) as (keyof typeof GroupFeatures)[]
-        if (!flags.length) {
-            const sections: proto.ISection[] = []
-            let text = '☘️ *Hii there 🎉* ☘️'
-            for (const feature of features) {
-                const rows: proto.IRow[] = []
-                rows.push(
-                    {
-                        title: `Help`,
-                        rowId: `${this.client.config.prefix}help`
-                    },
-                    {
-                        title: `Info`,
-                        rowId: `${this.client.config.prefix}info`
-                    }
-                )
-                sections.push({ title: this.client.utils.capitalize(category)} rows })
-                text += ``\`\`\`${categoryCommands.join(', ')}\`\`\```
+    public override execute = async (M: Message, { context }: IArgs): Promise<void> => {
+        if (!context) {
+            let commands = Array.from(this.handler.commands, ([command, data]) => ({
+                command,
+                data
+            })).filter((command) => command.data.config.category !== 'dev')
+            const { nsfw } = await this.client.DB.getGroup(M.from)
+            if (!nsfw) commands = commands.filter(({ data }) => data.config.category !== 'nsfw')
+            const buffer = await this.client.utils.getBuffer('https://telegra.ph/file/bd0f718f51e806ea1584e.mp4')
+            let text = `🎉 Hi there! 👋🏻 *@${M.sender.jid.split('@')[0]}*, I'm ${
+                this.client.config.name
+            }\n\n💠 *prefix:* "${this.client.config.prefix}"\n\n🔰 *Commands:* ${this.handler.commands.size}\n\n⛩️ *Categories:* 10`
+            const categories: string[] = []
+            for (const command of commands) {
+                if (categories.includes(command.data.config.category)) continue
+                categories.push(command.data.config.category)
             }
-            return void M.reply(
-                text,
-                'text',
-                undefined,
-                undefined,
-                undefined,
-                undefined,
-                undefined,
-                undefined,
-                undefined,
-                {
-                    sections,
-                    buttonText: 'Group Features'
-                }
-            )
+            for (const category of categories) {
+                const categoryCommands: string[] = []
+                const filteredCommands = commands.filter((command) => command.data.config.category === category)
+                text += `\n\n*〖･${this.client.utils.capitalize(category)}･〗*\n\n`
+                filteredCommands.forEach((command) => categoryCommands.push(command.data.name))
+                text += `${categoryCommands.join(', ')}`
+            }
+            text += `\n\n📔 *Note:* Use ${this.client.config.prefix}help <command_name> for more info of a specific command\n\n⭐ *Example:* *${this.client.config.prefix}help hello*`
+            return void (await M.reply(buffer, 'video', true, undefined, text, [M.sender.jid]))
         } else {
             const cmd = context.trim().toLowerCase()
             const command = this.handler.commands.get(cmd) || this.handler.aliases.get(cmd)
@@ -55,13 +45,13 @@ export default class extends BaseCommand {
                     !command.config.aliases
                         ? ''
                         : command.config.aliases.map((alias) => this.client.utils.capitalize(alias)).join(', ')
-                }\n🔗 *Category:* ${this.client.utils.capitalize(command.config.category)}\n⏰ *Cooldown:* ${
+                }\n🀄 *Category:* ${this.client.utils.capitalize(command.config.category)}\n⏰ *Cooldown:* ${
                     command.config.cooldown ?? 3
-                }s\n🎗 *Usage:* ${command.config.usage
+                }s\n💮 *Usage:* ${command.config.usage
                     .split('||')
                     .map((usage) => `${this.client.config.prefix}${usage.trim()}`)
                     .join(' | ')}\n🧧 *Description:* ${command.config.description}`
             )
-        }
+        } 
     }
 }
