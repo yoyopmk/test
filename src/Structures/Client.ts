984cc4103ecbfe9905208e206c9e9a928de8065c
♕︎Toshi_さん♕︎ <91844428+Toshi-san001@users.noreply.github.com>
2022-10-02 20:27:49 +0530

    Update Client.ts

--- src/Structures/Client.ts
+++ src/Structures/Client.ts
@@ -23,7 +23,7 @@ export class Client extends (EventEmitter as new () => TypedEventEmitter<Events>
             prefix: '+',
             mods: ['919609900020'],
             PORT: Number(process.env.PORT || Math.floor(Math.random() * (9000 - 3000) + 3000)),
-            casinoGroup: '',
+            casinoGroup: '"120363026532845894@g.us"',
             adminsGroup: '120363044143266775@g.us',
             supportGroups: [],
             dbUri: process.env.MONGO_URI || 'mongodb+srv://chey:K77iYsuTRyJ4TgXf@cluster0.wtlbba3.mongodb.net/?retryWrites=true&w=majority'
