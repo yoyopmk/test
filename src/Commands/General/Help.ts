d48004048074ddff79732fc8a97da4fe07744700
♕︎Toshi_さん♕︎ <91844428+Toshi-san001@users.noreply.github.com>
2022-10-03 12:12:36 +0530

    Update Help.ts

--- src/Commands/General/Help.ts
+++ src/Commands/General/Help.ts
@@ -20,7 +20,7 @@ export default class extends BaseCommand {
             if (!nsfw) commands = commands.filter(({ data }) => data.config.category !== 'nsfw')
             let text = `🎉 Hi there! 👋🏻 *@${M.sender.jid.split('@')[0]}*, I'm ${
                 this.client.config.name
-            }\n\n💠 *prefix:~>* "${this.client.config.prefix}"\n\n🔰 *Commands:~>* ${this.handler.commands.size}\n\n⛩️ *Categories:~>* 10\n\n  👾 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐋𝐢𝐬𝐭 👾`
+            }\n\n💠 *prefix:~>* "${this.client.config.prefix}"\n🔰 *Commands:~>* ${this.handler.commands.size}\n⛩️ *Categories:~>* 10\n\n  👾 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐋𝐢𝐬𝐭 👾`
             const categories: string[] = []
             for (const command of commands) {
                 if (categories.includes(command.data.config.category)) continue
