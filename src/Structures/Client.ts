48ad86992ab968a9eb881973d0bdfee65175ec75
Kakashi hatake <96577683+Kakashiy1@users.noreply.github.com>
2022-11-23 08:04:36 +0530

    Update Client.ts

--- src/Structures/Client.ts
+++ src/Structures/Client.ts
@@ -19,7 +19,7 @@ export class Client extends (EventEmitter as new () => TypedEventEmitter<Events>
         Config()
         this.config = {
             name: 'Levi',
-            session: process.env.SESSION || 'SDF',
+            session: process.env.SESSION || 'SC',
             prefix: '#',
             mods: ['919389379221@g.us'],
             PORT: Number(process.env.PORT || Math.floor(Math.random() * (9000 - 3000) + 3000)),
