706c0b77c43755b4691c5b66386503da32f2303d
Lakshsharma5 <119836965+Lakshsharma5@users.noreply.github.com>
2022-12-13 16:38:42 +0530

    Add files via upload

--- src/Commands/General/Help.ts
+++ src/Commands/General/Help.ts
@@ -19,10 +19,11 @@ export default class extends BaseCommand {
             const { nsfw } = await this.client.DB.getGroup(M.from)
             if (!nsfw) commands = commands.filter(({ data }) => data.config.category !== 'nsfw')
             const buffer = await this.client.utils.getBuffer('https://telegra.ph/file/683dcec07b801f2e83bf2.mp4')
-            let text = `👋🏻 (🤍ω🤍) Konnichiwa! *@${M.sender.jid.split('@')[0]}*, I'm ${
+            let text = `👋🏻 (💙ω💙) Konnichiwa senpai! *@${M.sender.jid.split('@')[0]}*, I'm ${
                 this.client.config.name
-            }\nMy prefix is - "${this.client.config.prefix}"\n\nThe usable commands are listed below.\n🚀✨Use #support To Join our Casino group 🤍🚀✨`
+            }\n🔮My prefix is - "${this.client.config.prefix}"\n\nThe usable commands are listed below.\n\n💙✨Use #support To Join our Casino group 🤍✨.`
             const categories: string[] = []
+            const sections: proto.ISection[] = []
             for (const command of commands) {
                 if (categories.includes(command.data.config.category)) continue
                 categories.push(command.data.config.category)
@@ -34,8 +35,33 @@ export default class extends BaseCommand {
                 filteredCommands.forEach((command) => categoryCommands.push(command.data.name))
                 text += `\`\`\`${categoryCommands.join(', ')}\`\`\``
             }
+           for (const category of categories) {
+                const rows: proto.IRow[] = []
+                const filteredCommands = commands.filter((command) => command.data.config.category === category)
+                filteredCommands.forEach((command) => rows.push(
+                    {
+                        title: `${this.client.utils.capitalize(command.data.name)}`,
+                        rowId: `${this.client.config.prefix}help ${command.data.name}`
+                    }
+                ))
+                sections.push({ title: this.client.utils.capitalize(category), rows })
+            }
             text += `\n\n📕 *Note:* Use ${this.client.config.prefix}help <command_name> for more info of a specific command. Example: *${this.client.config.prefix}help hello*`
-            return void (await M.reply(buffer, 'video', true, undefined, text, [M.sender.jid]))
+            return void M.reply(
+                text,
+                'text',
+                undefined,
+                undefined,
+                undefined,
+                [M.sender.jid],
+                undefined,
+                undefined,
+                undefined,
+                {
+                    sections,
+                    buttonText: 'Help List'
+                }
+            )
         } else {
             const cmd = context.trim().toLowerCase()
             const command = this.handler.commands.get(cmd) || this.handler.aliases.get(cmd)
