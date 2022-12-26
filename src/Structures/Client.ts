fa0fcd7660166674f3603496b45230fa431389d2
Lundbchuh <121439135+Lundbchuh@users.noreply.github.com>
2022-12-26 18:44:39 +0530

    Update Client.ts

--- src/Structures/Client.ts
+++ src/Structures/Client.ts
@@ -19,7 +19,7 @@ export class Client extends (EventEmitter as new () => TypedEventEmitter<Events>
         Config()
         this.config = {
             name: 'Levi',
-            session: process.env.SESSION || 'browser',
+            session: process.env.SESSION || 'Levibro',
             prefix: '-',
             mods: ['919389379221@g.us'],
             PORT: Number(process.env.PORT || Math.floor(Math.random() * (9000 - 3000) + 3000)),
